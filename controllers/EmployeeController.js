a696a535be37b8691a12be6672fc9722b4d26c59
yensubldg <thang11a5tp@gmail.com>
2022-02-03 01:15:57 +0700

    dev employee (form data object)

--- controllers/EmployeeController.js
+++ controllers/EmployeeController.js
@@ -1,152 +1,153 @@
 const { Employees } = require("../models");
 
 const getAll = async (req, res) => {
-    try {
-        const employees = await Employees.findAll();
-        res.json({ success: true, message: "Get all employees ok", employees });
-    } catch (error) {
-        console.log(error);
-        return res
-            .status(401)
-            .json({ success: false, message: "Get employees false" });
-    }
+
+  try {
+    const employees = await Employees.findAll();
+    res.json({ success: true, message: "Get all employees ok", employees });
+  } catch (error) {
+    console.log(error);
+    return res
+      .status(401)
+      .json({ success: false, message: "Get employees false" });
+  }
 };
 
 const getEmployee = async (req, res) => {
-    try {
-        const id = req.params.id;
-        const employee = await Employees.findOne({ where: { id } });
-        res.json({ success: true, message: "Get employee ok", employee });
-    } catch (error) {
-        console.log(error);
-        return res
-            .status(401)
-            .json({ success: false, message: "Get employee false" });
-    }
+  try {
+    const id = req.params.id;
+    const employee = await Employees.findOne({ where: { id } });
+    res.json({ success: true, message: "Get employee ok", employee });
+  } catch (error) {
+    console.log(error);
+    return res
+      .status(401)
+      .json({ success: false, message: "Get employee false" });
+  }
 };
 
 const addEmployee = async (req, res) => {
-    const {
-        name,
-        listphone,
-        birthday,
-        identification,
-        gender,
-        avatar,
-        address,
-        ability_work,
-        note,
-        blacklist,
-        ideti_file,
-    } = req.body;
-    try {
-        const newEmployee = {
-            name,
-            listphone,
-            birthday,
-            identification,
-            gender: gender || false,
-            avatar,
-            address,
-            ability_work,
-            note,
-            blacklist,
-            ideti_file,
-        };
-        await Employees.create(newEmployee);
-        return res.json({
-            success: true,
-            message: "New employee successfully created",
-            employee: newEmployee,
-        });
-    } catch (error) {
-        console.log("error " + error);
-        return res
-            .status(500)
-            .json({ success: false, message: "Internal server error" });
-    }
+  const {
+    name,
+    listphone,
+    birthday,
+    identification,
+    gender,
+    avatar,
+    address,
+    ability_work,
+    note,
+    blacklist,
+    ideti_file,
+  } = req.body;
+  try {
+    const newEmployee = {
+      name,
+      listphone,
+      birthday: birthday || null,
+      identification,
+      gender: gender || false,
+      avatar,
+      address: JSON.parse(address) || null,
+      ability_work,
+      note,
+      blacklist: blacklist || false,
+      ideti_file,
+    };
+    await Employees.create(newEmployee);
+    return res.json({
+      success: true,
+      message: "New employee successfully created",
+      employee: newEmployee,
+    });
+  } catch (error) {
+    console.log("error " + error);
+    return res
+      .status(500)
+      .json({ success: false, message: "Internal server error" });
+  }
 };
 const updateEmployee = async (req, res) => {
-    const {
-        name,
-        listphone,
-        birthday,
-        identification,
-        gender,
-        avatar,
-        address,
-        ability_work,
-        note,
-        blacklist,
-        ideti_file,
-    } = req.body;
-    try {
-        const conditionUpdateEmployee = {
-            id: req.params.id,
-        };
-        let updateEmployee = {
-            name,
-            listphone,
-            birthday,
-            identification,
-            gender,
-            avatar,
-            address,
-            ability_work,
-            note,
-            blacklist,
-            ideti_file,
-        };
-        updateEmployee = await Employees.update(updateEmployee, {
-            where: conditionUpdateEmployee,
-        });
+  const {
+    name,
+    listphone,
+    birthday,
+    identification,
+    gender,
+    avatar,
+    address,
+    ability_work,
+    note,
+    blacklist,
+    ideti_file,
+  } = req.body;
+  try {
+    const conditionUpdateEmployee = {
+      id: req.params.id,
+    };
+    let updateEmployee = {
+      name,
+      listphone,
+      birthday,
+      identification,
+      gender,
+      avatar,
+      address,
+      ability_work,
+      note,
+      blacklist,
+      ideti_file,
+    };
+    updateEmployee = await Employees.update(updateEmployee, {
+      where: conditionUpdateEmployee,
+    });
 
-        if (!updateEmployee)
-            return res.status(401).json({
-                success: false,
-                message: "Update employee failed",
-            });
-        res.json({ success: true, message: "Update employee successfully" });
-    } catch (error) {
-        console.log("error " + error);
-        return res
-            .status(500)
-            .json({ success: false, message: "Internal server error" });
-    }
+    if (!updateEmployee)
+      return res.status(401).json({
+        success: false,
+        message: "Update employee failed",
+      });
+    res.json({ success: true, message: "Update employee successfully" });
+  } catch (error) {
+    console.log("error " + error);
+    return res
+      .status(500)
+      .json({ success: false, message: "Internal server error" });
+  }
 };
 
 const deleteEmployee = async (req, res) => {
-    try {
-        const conditionDeleteEmployee = {
-            id: req.params.id,
-        };
+  try {
+    const conditionDeleteEmployee = {
+      id: req.params.id,
+    };
 
-        const deleteEmployee = await Employees.destroy({
-            where: conditionDeleteEmployee,
-        });
+    const deleteEmployee = await Employees.destroy({
+      where: conditionDeleteEmployee,
+    });
 
-        if (!deleteEmployee)
-            return res.status(401).json({
-                success: false,
-                message: "Delete false",
-            });
-        res.json({
-            success: true,
-            message: "Delete ok",
-            employee: deleteEmployee,
-        });
-    } catch (error) {
-        console.log("error " + error);
-        return res
-            .status(500)
-            .json({ success: false, message: "Internal server error" });
-    }
+    if (!deleteEmployee)
+      return res.status(401).json({
+        success: false,
+        message: "Delete false",
+      });
+    res.json({
+      success: true,
+      message: "Delete ok",
+      employee: deleteEmployee,
+    });
+  } catch (error) {
+    console.log("error " + error);
+    return res
+      .status(500)
+      .json({ success: false, message: "Internal server error" });
+  }
 };
 
 module.exports = {
-    getAll,
-    getEmployee,
-    addEmployee,
-    updateEmployee,
-    deleteEmployee,
+  getAll,
+  getEmployee,
+  addEmployee,
+  updateEmployee,
+  deleteEmployee,
 };
--- routes/employee.js
+++ routes/employee.js
@@ -4,10 +4,27 @@ const verifyToken = require("../middlewares/auth");
 const EmployeeController = require("../controllers/EmployeeController");
 const { validate } = require("../services/validator");
 const { body } = require("express-validator");
+const multer = require("multer");
+const upload = multer({
+  storage: multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, "./uploads/");
+    },
+    filename: function (req, file, cb) {
+      console.log(file);
+      cb(null, `${Date.now()}_${file.originalname}`);
+    },
+  }),
+});
+
+
+route.use(express.json());
+route.use(express.urlencoded({ extended: true }));
+// route.use(formidable());
 
 const validateEmployee = [
-    body("name", "Invalid name").not().isEmpty(),
-    // body("phone", "Invalid phone").isMobilePhone("vi-VN"),
+  body("name", "Invalid name").not().isEmpty(),
+  // body("phone", "Invalid phone").isMobilePhone("vi-VN"),
 ];
 
 //@route GET api/employee
@@ -20,18 +37,18 @@ route.get("/:id", verifyToken, EmployeeController.getEmployee);
 
 //@ route POST api/employee/add
 route.post(
-    "/add",
-    verifyToken,
-    validate(validateEmployee),
-    EmployeeController.addEmployee
+  "/add",
+  upload.single("avatar"),
+  // upload.array("ideti_file", 12),
+  EmployeeController.addEmployee
 );
 
 //@ route PUT api/employee/edit/id
 route.put(
-    "/edit/:id",
-    verifyToken,
-    validate(validateEmployee),
-    EmployeeController.updateEmployee
+  "/edit/:id",
+  verifyToken,
+  validate(validateEmployee),
+  EmployeeController.updateEmployee
 );
 
 //@ route DELETE api/employee/delete/id
