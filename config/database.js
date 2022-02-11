ccb44b3e46e541aa8c05cdc2f0e62108109a776c
kimdat546 <kimdat546@gmail.com>
2022-02-11 22:20:39 +0700

    set timezone sequelize

--- config/database.js
+++ config/database.js
@@ -1,24 +1,27 @@
 require("dotenv").config();
 module.exports = {
-    development: {
-        username: process.env.USERMYSQL,
-        password: process.env.PASSMYSQL,
-        database: process.env.DATABASE,
-        host: process.env.HOST,
-        dialect: process.env.DIALECT,
-    },
-    test: {
-        username: process.env.USERMYSQL,
-        password: process.env.PASSMYSQL,
-        database: process.env.DATABASE,
-        host: process.env.HOST,
-        dialect: process.env.DIALECT,
-    },
-    production: {
-        username: process.env.USERMYSQL,
-        password: process.env.PASSMYSQL,
-        database: process.env.DATABASE,
-        host: process.env.HOST,
-        dialect: process.env.DIALECT,
-    },
+	development: {
+		username: process.env.USERMYSQL,
+		password: process.env.PASSMYSQL,
+		database: process.env.DATABASE,
+		host: process.env.HOST,
+		dialect: process.env.DIALECT,
+		timezone: process.env.TIMEZONE,
+	},
+	test: {
+		username: process.env.USERMYSQL,
+		password: process.env.PASSMYSQL,
+		database: process.env.DATABASE,
+		host: process.env.HOST,
+		dialect: process.env.DIALECT,
+		timezone: process.env.TIMEZONE,
+	},
+	production: {
+		username: process.env.USERMYSQL,
+		password: process.env.PASSMYSQL,
+		database: process.env.DATABASE,
+		host: process.env.HOST,
+		dialect: process.env.DIALECT,
+		timezone: process.env.TIMEZONE,
+	},
 };
--- test/connectDB.js
+++ test/connectDB.js
@@ -23,6 +23,7 @@ const sequelize = new Sequelize(
     {
         host: process.env.HOST,
         dialect: process.env.DIALECT,
+		timezone: process.env.TIMEZONE,
         logging: false,
     }
 );
