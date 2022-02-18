6c3752f8d9c7d6c20c50b2ee3969cf6bbfeb4344
yensubldg <thang11a5tp@gmail.com>
2022-02-18 21:31:19 +0700

    improve route

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const rfs = require("rotating-file-stream");
 const connectDB = require("./test/connectDB");
 const db = require("./models");
 const dotenv = require("dotenv");
+var fs = require("fs");
 const { verifyToken } = require("./middlewares/auth");
 dotenv.config();
 
@@ -27,7 +28,20 @@ app.use(helmet());
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use("/uploads", verifyToken, express.static("uploads"));
+
+var mime = {
+	html: "text/html",
+	txt: "text/plain",
+	css: "text/css",
+	gif: "image/gif",
+	jpg: "image/jpeg",
+	png: "image/png",
+	svg: "image/svg+xml",
+	js: "application/javascript",
+};
+
+const dir = path.join(__dirname, "uploads");
+app.use("/uploads", express.static(dir));
 
 connectDB();
 // db.sequelize.sync().then(() => {
