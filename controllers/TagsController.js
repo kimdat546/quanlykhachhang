d9232eabc0b8137889cee45cf0e91e1c59aa8215
yensubldg <thang11a5tp@gmail.com>
2022-02-28 23:54:02 +0700

    done Tags Controller

--- controllers/TagsController.js
+++ controllers/TagsController.js
@@ -1,6 +1,5 @@
 const { Tags } = require("../models");
 const { Op } = require("sequelize");
-const res = require("express/lib/response");
 
 /**
  * @param {string} tag
@@ -13,14 +12,14 @@ const checkTagExist = async (tag_name, tag) => {
 			tag_name: tag_name,
 		},
 	});
-	if (existTag.list_tags.includes(tag)) {
+	if (existTag.list_tags && existTag.list_tags.includes(tag)) {
 		return true;
 	}
 	return false;
 };
 
 /**
- * @param {string} tag
+ * @param {string} tag, tag_name
  * @description add tag to list_tags and include old tag
  */
 const addTag = async (req, res) => {
@@ -37,7 +36,10 @@ const addTag = async (req, res) => {
 		});
 		await Tags.update(
 			{
-				list_tags: [...list_tags.list_tags, tag],
+				list_tags:
+					list_tags.list_tags && list_tags.list_tags.length > 0
+						? [...list_tags.list_tags, tag]
+						: [tag],
 			},
 			{
 				where: {
@@ -77,7 +79,93 @@ const getAllTagsByTagName = async (req, res) => {
 	}
 };
 
+/**
+ * @param {string} tag, tag_name
+ * @description delete tag from list_tags
+ */
+
+const deleteTag = async (req, res) => {
+	const { tag, tag_name } = req.body;
+	const existTag = await checkTagExist(tag_name, tag);
+	if (existTag)
+		try {
+			const list_tags = await Tags.findOne({
+				where: {
+					tag_name: tag_name,
+				},
+			});
+			const newListTags = list_tags.list_tags.filter(
+				(item) => item !== tag
+			);
+			await Tags.update(
+				{
+					list_tags: newListTags,
+				},
+				{
+					where: {
+						tag_name: tag_name,
+					},
+				}
+			);
+			return res.json({
+				success: true,
+				message: "Delete tag ok",
+			});
+		} catch (error) {
+			console.log(error);
+			return res
+				.status(401)
+				.json({ success: false, message: "Delete tag false" });
+		}
+	return res.json({
+		success: false,
+		message: "Tag not exist",
+	});
+};
+
+/**
+ * @param {string}  tag_name
+ * @description delete all tags by tag_name
+ */
+const deleteAllTagsByTagName = async (req, res) => {
+	const { tag_name } = req.body;
+	try {
+		const tags = await Tags.findOne({
+			where: {
+				tag_name: tag_name,
+			},
+		});
+		if (tags.list_tags === null || tags.list_tags.length === 0) {
+			return res.json({
+				success: false,
+				message: "Tag is empty",
+			});
+		}
+		await Tags.update(
+			{
+				list_tags: null,
+			},
+			{
+				where: {
+					tag_name: tag_name,
+				},
+			}
+		);
+		return res.json({
+			success: true,
+			message: "Delete all tags ok",
+		});
+	} catch (error) {
+		console.log(error);
+		return res
+			.status(401)
+			.json({ success: false, message: "Delete all tags false" });
+	}
+};
+
 module.exports = {
 	addTag,
 	getAllTagsByTagName,
+	deleteTag,
+	deleteAllTagsByTagName,
 };
--- routes/tags.js
+++ routes/tags.js
@@ -4,7 +4,7 @@ const { verifyToken } = require("../middlewares/auth");
 const TagsController = require("../controllers/TagsController");
 
 /**
- * @param {string} tag
+ * @param {string} tag, tag_name
  * @returns {object}
  * @description add tag to list_tags
  */
@@ -16,5 +16,17 @@ route.post("/add", verifyToken, TagsController.addTag);
  * @description get all tags
  */
 route.post("/getByTagName", verifyToken, TagsController.getAllTagsByTagName);
+
+/**
+ * @param {string} tag, tag_name
+ * @description delete tag from list_tags
+ */
+route.post("/delete", verifyToken, TagsController.deleteTag);
+
+/**
+ * @param {string} tag_name
+ * @description delete all tags by tag_name
+ */
+route.post("/deleteAll", verifyToken, TagsController.deleteAllTagsByTagName);
 
 module.exports = route;
