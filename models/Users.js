0d52539d773fcbf286c49bc710f99cac7a5c8712
kimdat546 <kimdat546@gmail.com>
2022-03-12 20:25:36 +0700

    fix datatype array enum

--- models/Users.js
+++ models/Users.js
@@ -38,28 +38,27 @@ module.exports = function (sequelize, DataTypes) {
 				defaultValue: "both",
 			},
 			authorization: {
-				type: DataTypes.ARRAY(
-					DataTypes.ENUM(
-						1, //Xem lao dong he thong
-						2, //Xem lao dong tk chinh minh
-						3, //Xem lao dong cac tk con
-						4, //Xem khach hang he thong
-						5, //Xem khach hang tk chinh minh
-						6, //Xem khach hang cac tk con
-						7, //Xem hop dong he thong
-						8, //Xem hop dong tk chinh minh
-						9, //Xem hop dong cac tk con
-						10, //Sua lao dong he thong
-						11, //Sua lao dong tk chinh minh
-						12, //Sua lao dong cac tk con
-						13, //Sua khach hang he thong
-						14, //Sua khach hang tk chinh minh
-						15, //Sua khach hang cac tk con
-						16, //Sua hop dong he thong
-						17, //Sua hop dong tk chinh minh
-						18 //Sua hop dong cac tk con
-					)
-				),
+				/**
+				1, //Xem lao dong he thong
+				2, //Xem lao dong tk chinh minh
+				3, //Xem lao dong cac tk con
+				4, //Xem khach hang he thong
+				5, //Xem khach hang tk chinh minh
+				6, //Xem khach hang cac tk con
+				7, //Xem hop dong he thong
+				8, //Xem hop dong tk chinh minh
+				9, //Xem hop dong cac tk con
+				10, //Sua lao dong he thong
+				11, //Sua lao dong tk chinh minh
+				12, //Sua lao dong cac tk con
+				13, //Sua khach hang he thong
+				14, //Sua khach hang tk chinh minh
+				15, //Sua khach hang cac tk con
+				16, //Sua hop dong he thong
+				17, //Sua hop dong tk chinh minh
+				18 //Sua hop dong cac tk con
+				*/
+				type: DataTypes.JSON,
 				allowNull: true,
 			},
 			refreshToken: {
