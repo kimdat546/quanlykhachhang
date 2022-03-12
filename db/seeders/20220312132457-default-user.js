327f286b57722de9b52da79021dbfd4cc4669179
yensubldg <thang11a5tp@gmail.com>
2022-03-12 20:35:26 +0700

    test create seeds

---
+++ db/seeders/20220312132457-default-user.js
@@ -0,0 +1,41 @@
+"use strict";
+
+module.exports = {
+	async up(queryInterface, Sequelize) {
+		/**
+		 * Add seed commands here.
+		 *
+		 * Example:
+		 * await queryInterface.bulkInsert('People', [{
+		 *   name: 'John Doe',
+		 *   isBetaMember: false
+		 * }], {});
+		 */
+		await queryInterface.bulkInsert(
+			"Users",
+			[
+				{
+					id: "7328ca6e-78f4-492c-b704-45d52aa2e257",
+					name: "Admin",
+					username: "thienphuc",
+					password: "@QuanLy1968",
+					email: "windev.thang@gmail.com",
+					role: "admin",
+					createdAt: new Date(),
+					updatedAt: new Date(),
+				},
+			],
+			{}
+		);
+	},
+
+	async down(queryInterface, Sequelize) {
+		/**
+		 * Add commands to revert seed here.
+		 *
+		 * Example:
+		 * await queryInterface.bulkDelete('People', null, {});
+		 */
+		await queryInterface.bulkDelete("Users", null, {});
+	},
+};
